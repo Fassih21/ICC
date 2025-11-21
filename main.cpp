dcb15eff5c652b8314788b262a1cc22c84f07006
Fassih21 <fassihkamran880@gmail.com>
2025-11-21 18:56:13 +0500

    Added players class

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Player{
+    private:
+        int playerID;
+        string name;
+        int age;
+        string role;
+        int matches;
+        int runs;
+        int wickets;
+        
+};
