b9fe1f8c68d53a4d58bd077b3ccfd60096022de0
Fassih21 <fassihkamran880@gmail.com>
2025-12-07 15:33:22 +0500

    Remove errors

--- main.cpp
+++ main.cpp
@@ -1,73 +1,76 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
-class Player{
-    private:
-        int playerID;
-        string name;
-        int age;
-        string role;
-        int matches;
-        int runs;
-        int wickets;
-        public:
-        Player(){
-            playerID=0;
-            name="";
-            age=0;
-            role="";
-            matches=0;
-            runs=0;
-            wickets=0;
-        }
-         int getPlayerID() {
+
+class Player {
+private:
+    int playerID;
+    string name;
+    int age;
+    string role;
+    int matches;
+    int runs;
+    int wickets;
+
+public:
+    Player() {
+        playerID = 0;
+        name = "";
+        age = 0;
+        role = "";
+        matches = 0;
+        runs = 0;
+        wickets = 0;
+    }
+
+    int getPlayerID() {
         return playerID;
-        }   
-        
-        void AddPlayer(int playerID, string name, int age, string role, int matches, int runs, int wickets){
-            cout << "---Adding Player Details---" << endl;
-            cout << "Enter Player ID: " << endl;
-            cin  >> playerID;
-            cout << "Enter Player's Name: " << endl;
-            cin >> name;
-            cout << "Enter Player's age: " << endl;
-            cin >> age;
-            cout << "Enter Player's Role: " << endl;
-            cin >> role;
-            cout << "Enter Number of Matches Played: " << endl;
-            cin >> matches;
-            cout << "Enter Total Runs Scored: " << endl;
-            cin >> runs;
-            cout << "Enter Total Wickets Taken: " << endl;
-            cin >> wickets;
-        }
-        void DisplayPlayer(){
-            cout << "---Player Details---" << endl;
-            cout << "Player ID: " << playerID << endl;
-            cout << "Player's Name: " << name << endl;
-            cout << "Player's Age: " << age << endl;
-            cout << "Player's Role: " << role << endl;
-            cout << "Number of Matches Played: " << matches << endl;
-            cout << "Total Runs Scored: " << runs << endl;
-            cout << "Total Wickets Taken: " << wickets << endl;
-        }
-       void UpdateStats() {
-            cout << "---Updating Player Stats---" << endl;
+    }
 
-            int addRuns;
-            cout << "Enter Additional Runs Scored: ";
-            cin >> addRuns;
-            this->runs += addRuns;
+    void AddPlayer() {
+        cout << "---Adding Player Details---" << endl;
+        cout << "Enter Player ID: ";
+        cin >> playerID;
+        cin.ignore();
+        cout << "Enter Player's Name: ";
+        getline(cin, name);
+        cout << "Enter Player's Age: ";
+        cin >> age;
+        cin.ignore();
+        cout << "Enter Player's Role: ";
+        getline(cin, role);
+        cout << "Enter Number of Matches Played: ";
+        cin >> matches;
+        cout << "Enter Total Runs Scored: ";
+        cin >> runs;
+        cout << "Enter Total Wickets Taken: ";
+        cin >> wickets;
+    }
 
-            int addWickets;
-            cout << "Enter Additional Wickets Taken: ";
-            cin  >> addWickets;
-            this->wickets += addWickets;
+    void DisplayPlayer() {
+        cout << "---Player Details---" << endl;
+        cout << "Player ID: " << playerID << endl;
+        cout << "Player's Name: " << name << endl;
+        cout << "Player's Age: " << age << endl;
+        cout << "Player's Role: " << role << endl;
+        cout << "Number of Matches Played: " << matches << endl;
+        cout << "Total Runs Scored: " << runs << endl;
+        cout << "Total Wickets Taken: " << wickets << endl;
+    }
 
-            cout << "Updated Total Runs Scored: " << this->runs << endl;
-            cout << "Updated Total Wickets Taken: " << this->wickets << endl;
-}
-
+    void UpdateStats() {
+        cout << "---Updating Player Stats---" << endl;
+        int addRuns;
+        cout << "Enter Additional Runs Scored: ";
+        cin >> addRuns;
+        runs += addRuns;
+        int addWickets;
+        cout << "Enter Additional Wickets Taken: ";
+        cin >> addWickets;
+        wickets += addWickets;
+        cout << "Updated Total Runs Scored: " << runs << endl;
+        cout << "Updated Total Wickets Taken: " << wickets << endl;
+    }
 };
 
 class Team {
@@ -85,25 +88,22 @@ public:
     }
 
     void AddTeam() {
-        cout << "---Adding Team Details---\n";
-
+        cout << "---Adding Team Details---" << endl;
         cout << "Enter Team ID: ";
         cin >> TeamID;
-
+        cin.ignore();
         cout << "Enter Team Name: ";
-        cin >> teamname;
-
+        getline(cin, teamname);
         cout << "Enter Country: ";
-        cin >> country;
-
+        getline(cin, country);
         cout << "\nEnter 11 Players:\n";
         for (int i = 0; i < 11; i++) {
-            cout << "\nPlayer " << (i+1) << ":\n";
-            listofplayers[i].AddPlayer(0, "", 0, "", 0, 0, 0);
+            cout << "\nPlayer " << (i + 1) << ":\n";
+            listofplayers[i].AddPlayer();
         }
     }
 
-    void addplayertoTeam(Player p) {
+    void addPlayerToTeam(Player p) {
         for (int i = 0; i < 11; i++) {
             if (listofplayers[i].getPlayerID() == 0) {
                 listofplayers[i] = p;
@@ -114,11 +114,10 @@ public:
     }
 
     void DisplayTeam() {
-        cout << "---Team Details---\n";
+        cout << "---Team Details---" << endl;
         cout << "Team ID: " << TeamID << endl;
         cout << "Team Name: " << teamname << endl;
         cout << "Country: " << country << endl;
-
         for (int i = 0; i < 11; i++) {
             listofplayers[i].DisplayPlayer();
             cout << endl;
@@ -126,35 +125,57 @@ public:
     }
 };
 
-class CricketBoard{
-    private:
+class CricketBoard {
+private:
     int boardID;
     string boardName;
     string country;
     int listofteams;
-    public:
-    CricketBoard(){
-        boardName="No name";
-        country="No country";
-        listofteams=0;
-        boardID=0;
+
+public:
+    CricketBoard() {
+        boardID = 0;
+        boardName = "No name";
+        country = "No country";
+        listofteams = 0;
     }
 
-    void AddBoard(int boardID, string boardName, string country, int listofteams){
-        cout << "----Details----";
-        cout << "Enter the Name of New Cricket Board: ";
-        cin >> boardName;
-        cout << "Enter the Id of your Cricket Board:";
+    void AddBoard() {
+        cout << "----Adding Cricket Board Details----" << endl;
+        cin.ignore();
+        cout << "Enter Board Name: ";
+        getline(cin, boardName);
+        cout << "Enter Board ID: ";
         cin >> boardID;
-        cout << "Enter the country of your Cricket Board: ";
-        cin >> country;
-        cout << "How much teams are under your Cricket Board: ";
+        cin.ignore();
+        cout << "Enter Country: ";
+        getline(cin, country);
+        cout << "Enter Number of Teams under this Board: ";
         cin >> listofteams;
     }
-    void DisplayBoard(){
-        cout << "Your Board Name is: " << boardName << endl;
-        cout << "Your Board ID is: " << boardID << endl;
-        cout << "Your Board Country is: " << country << endl;
-        cout << "Number of Teams associated with your Board is: " << listofteams << endl;
+
+    void DisplayBoard() {
+        cout << "---Cricket Board Details---" << endl;
+        cout << "Board Name: " << boardName << endl;
+        cout << "Board ID: " << boardID << endl;
+        cout << "Country: " << country << endl;
+        cout << "Number of Teams: " << listofteams << endl;
     }
 };
+
+int main() {
+    Player p;
+    p.AddPlayer();
+    p.DisplayPlayer();
+    p.UpdateStats();
+
+    Team t;
+    t.AddTeam();
+    t.DisplayTeam();
+
+    CricketBoard cb;
+    cb.AddBoard();
+    cb.DisplayBoard();
+
+    return 0;
+}
